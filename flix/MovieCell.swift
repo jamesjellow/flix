d5c40c926fa58b5db10f4d94e7d1c98f7a561efa
jamesjellow <jellowjames@gmail.com>
2020-01-20 23:26:47 -0800

    Finished Project -added constraints -tested on person device

---
+++ flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Gabe De Mesa on 1/20/20.
+//  Copyright © 2020 Gabe De Mesa. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var synopsisLabel: UILabel!
+    @IBOutlet var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
